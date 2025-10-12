e4a555512bba97d2faca5d8ad2268cf3f44f51a1
yilka <yilkan0510@gmail.com>
2025-10-12 17:26:06 +0200

    verticaal gezet

--- viginereplus/main.cpp
+++ viginereplus/main.cpp
@@ -6,42 +6,32 @@
 
 using namespace std;
 
-// Functie om de kolommen naar een bestand te schrijven
+// Jouw writeColumnsToFile functie blijft ongewijzigd. Perfect voor visualisatie.
 void writeColumnsToFile(const vector<string>& columns, const string& filename, int num_columns) {
     ofstream outFile(filename);
     if (!outFile.is_open()) {
         cout << "Error: Kon output file niet aanmaken." << endl;
         return;
     }
-
-    // Bepaal het aantal rijen op basis van de eerste kolom (ervan uitgaande dat alle kolommen even lang zijn)
     int num_rows = columns.empty() ? 0 : columns[0].length();
-
-    // Header
     for (int i = 0; i < num_columns; ++i) {
         outFile << "KOLOM " << i << "    ";
     }
     outFile << endl;
-
     for (int i = 0; i < num_columns; ++i) {
         outFile << "-----------";
     }
     outFile << endl;
-
-    // Loop door elke rij
     for (int i = 0; i < num_rows; ++i) {
-        // Loop door elke kolom
         for (int j = 0; j < num_columns; ++j) {
-            // Controleer of de huidige rij en kolom geldig zijn
             if (j < columns.size() && i < columns[j].length()) {
                 outFile << columns[j][i] << "          ";
             } else {
-                outFile << "           "; // Lege ruimte voor kolommen met minder rijen
+                outFile << "           ";
             }
         }
         outFile << endl;
     }
-
     cout << "Kolommen succesvol weggeschreven naar " << filename << endl;
 }
 
@@ -60,14 +50,18 @@ int main() {
         cout << "File succesvol ingelezen!" << endl;
         cout << "Totaal aantal tekens: " << ciphertext.length() << endl;
 
-        // Pas hier het aantal kolommen aan
-        int num_columns = 9;
+        int num_columns = 10;
         vector<string> columns(num_columns);
 
-        for (int i = 0; i < ciphertext.length(); ++i) {
-            columns[i % num_columns] += ciphertext[i];
+        // --- DIT IS DE ENIGE AANPASSING ---
+        // We verdelen de ciphertext nu in 10 blokken van 139 tekens.
+        int col_length = ciphertext.length() / num_columns;
+        for (int i = 0; i < num_columns; ++i) {
+            columns[i] = ciphertext.substr(i * col_length, col_length);
         }
+        // ------------------------------------
 
+        // We gebruiken je bestaande functie om het resultaat te bekijken.
         writeColumnsToFile(columns, "../viginereplus/output_columns.txt", num_columns);
 
     } else {
